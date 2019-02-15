f9b6591275669b282d81128819500924e4a7acf0
Avi Cramer <abcramer@ucsd.edu>
2019-02-14 17:07:20 -0800

    final fix for heroku app vs localhost

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -29,7 +29,7 @@ function addProjectDetails(e) {
 	console.log("User clicked on project " + idNumber);
 
 	console.log("projectID = " + projectID);
-	var url = $.get("https://lab6-acramer.herokuapp.com/"+ idNumber, callback);
+	var url = $.get("https://lab6-acramer.herokuapp.com/project/"+ idNumber, callback);
 	console.log(url);
 	// /var htmlString = url.responseJSON[image]
 	//var htmlString = $.get(url);
